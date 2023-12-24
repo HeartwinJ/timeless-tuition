6435aa822dd9658b8efee47bb033ac7145955d3a
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2023-12-24 04:41:10 +0000

    Updated Footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -20,21 +20,35 @@ const socials = [
 
 <template>
   <div
-    class="sm:px-18 flex w-full flex-col items-center justify-between gap-3 bg-gray-200 px-8 py-5 sm:flex-row lg:px-48"
+    class="sm:px-18 flex w-full flex-col items-center justify-center gap-3 bg-gray-200 px-8 py-5 lg:px-48"
   >
-    <div class="text-gray-500">
-      &copy; {{ new Date().getFullYear() }} Timeless Tuition. All rights
-      reserved.
-    </div>
-    <div class="flex items-center gap-3">
-      <div v-for="(social, idx) in socials" :key="idx">
-        <a
-          :href="social.path"
-          target="_blank"
-          class="text-2xl text-gray-500 hover:text-gray-700"
+    <div
+      class="flex w-full flex-col items-center justify-between gap-3 sm:flex-row"
+    >
+      <div class="text-gray-500 text-center sm:text-left">
+        <RouterLink to="/legal" class="block hover:text-gray-700"
+          >Legal</RouterLink
         >
-          <component :is="social.icon" class="h-6 w-6" />
-        </a>
+        <RouterLink to="/terms" class="block hover:text-gray-700"
+          >Terms and Conditions</RouterLink
+        >
+      </div>
+      <div class="flex items-center gap-3">
+        <div v-for="(social, idx) in socials" :key="idx">
+          <a
+            :href="social.path"
+            target="_blank"
+            class="text-2xl text-gray-500 hover:text-gray-700"
+          >
+            <component :is="social.icon" class="h-6 w-6" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="text-gray-500">
+        &copy; {{ new Date().getFullYear() }} Timeless Tuition. All rights
+        reserved.
       </div>
     </div>
   </div>
