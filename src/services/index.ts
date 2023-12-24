34898d5978984b0b8b06a1b6f6756780138fa355
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2023-12-24 04:42:14 +0000

    Added CV Upload functionality

--- src/services/index.ts
+++ src/services/index.ts
@@ -10,6 +10,16 @@ interface QueriesTable {
   message: string;
 }
 
+interface ApplicationsTable {
+  opening_id: string;
+  cv_url: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  message: string;
+}
+
 const supabase = createClient(
   import.meta.env.TT_DB_URL,
   import.meta.env.TT_DB_KEY,
@@ -18,5 +28,17 @@ const supabase = createClient(
 export default {
   async saveContact(data: QueriesTable) {
     await supabase.from("queries").insert(data);
+  },
+  async uploadCV(file: File) {
+    const fileExt = file.name.split(".").pop();
+    const filePath = `${Math.random()}.${fileExt}`;
+    await supabase.storage.from("cvs").upload(filePath, file);
+    return filePath;
+  },
+  async saveApplication(data: ApplicationsTable) {
+    await supabase.from("applications").insert({
+      ...data,
+      cv_url: supabase.storage.from("cvs").getPublicUrl(data.cv_url).data.publicUrl,
+    });
   },
 };
--- src/views/CareersView.vue
+++ src/views/CareersView.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
-import { IconHourglass, IconCalendar, IconMapPin } from "@tabler/icons-vue";
+import { ref, reactive } from "vue";
+import { reset } from "@formkit/vue";
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  DialogDescription,
+} from "@headlessui/vue";
+import {
+  IconHourglass,
+  IconCalendar,
+  IconMapPin,
+  IconCircleArrowRight,
+} from "@tabler/icons-vue";
+import SpinnerComponent from "@/components/SpinnerComponent.vue";
+import Services from "@/services";
 
 const openings = [
   {
+    id: "1",
     department: "Mathematics",
     role: "Mathematics Tutor",
     description:
@@ -12,6 +28,7 @@ const openings = [
     location: "London",
   },
   {
+    id: "2",
     department: "Science",
     role: "Physics Teaching Assistant",
     description:
@@ -21,6 +38,7 @@ const openings = [
     location: "London",
   },
   {
+    id: "3",
     department: "Science",
     role: "Biology Tutor",
     description:
@@ -30,6 +48,7 @@ const openings = [
     location: "London",
   },
   {
+    id: "4",
     department: "Language",
     role: "English Tutor",
     description:
@@ -39,52 +58,205 @@ const openings = [
     location: "London",
   },
 ];
+
+const isApplyDialogOpen = ref(false);
+const isSuccessDialogOpen = ref(false);
+const isProcessing = ref(false);
+
+const formData = reactive({
+  opening_id: "",
+  cv_url: "",
+  firstname: "",
+  lastname: "",
+  email: "",
+  phone: "",
+  message: "",
+});
+
+function applyTo(openingId: string) {
+  formData.opening_id = openingId;
+  isApplyDialogOpen.value = true;
+}
+
+async function handleSubmit(data: any) {
+  isProcessing.value = true;
+  const cvUrl = await Services.uploadCV(data.cv_url[0].file);
+  await Services.saveApplication({
+    ...data,
+    cv_url: cvUrl,
+  });
+  reset("application-form");
+  isApplyDialogOpen.value = false;
+  isProcessing.value = false;
+  isSuccessDialogOpen.value = true;
+}
 </script>
 
 <template>
-  <div class="mx-auto max-w-7xl px-6 py-16 sm:py-20 lg:px-8 lg:py-24">
-    <div class="mx-auto max-w-2xl lg:max-w-none">
-      <div class="py-8 text-center">
-        <div class="font-medium text-pink-600">Careers</div>
-        <div class="text-xl font-bold uppercase">work with us</div>
-        <div>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet ut
-          consequuntur neque eum, porro enim labore quis aspernatur impedit ad
-          eos cupiditate non quibusdam numquam alias necessitatibus illo
-          corporis obcaecati. Lorem ipsum, dolor sit amet consectetur
-          adipisicing elit. Ducimus, reprehenderit ullam praesentium
-          accusantium, assumenda sequi possimus soluta iste sit ipsa laudantium
-          corrupti. Voluptas harum sit saepe. Modi provident pariatur explicabo.
+  <div>
+    <div class="mx-auto max-w-7xl px-6 py-16 sm:py-20 lg:px-8 lg:py-24">
+      <div class="mx-auto max-w-2xl lg:max-w-none">
+        <div class="py-8 text-center">
+          <div class="font-medium text-pink-600">Careers</div>
+          <div class="text-xl font-bold uppercase">work with us</div>
+          <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet ut
+            consequuntur neque eum, porro enim labore quis aspernatur impedit ad
+            eos cupiditate non quibusdam numquam alias necessitatibus illo
+            corporis obcaecati. Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Ducimus, reprehenderit ullam praesentium
+            accusantium, assumenda sequi possimus soluta iste sit ipsa
+            laudantium corrupti. Voluptas harum sit saepe. Modi provident
+            pariatur explicabo.
+          </div>
         </div>
-      </div>
-      <div
-        v-for="(opening, idx) in openings"
-        :key="idx"
-        class="m-3 flex rounded-lg border bg-white p-3 shadow"
-      >
-        <div>
-          <div class="mb-2 text-sm text-gray-700">
-            <span class="rounded-full border bg-white px-2 py-1">
-              {{ opening.department }}
-            </span>
+        <div
+          v-for="(opening, idx) in openings"
+          :key="idx"
+          class="m-3 flex rounded-lg border bg-white p-3 shadow"
+        >
+          <div>
+            <div class="mb-2 text-sm text-gray-700">
+              <span class="rounded-full border bg-white px-2 py-1">
+                {{ opening.department }}
+              </span>
+            </div>
+            <div class="text-lg font-medium text-pink-600">
+              {{ opening.role }}
+            </div>
+            <div class="text-gray-700">{{ opening.description }}</div>
           </div>
-          <div class="text-lg font-medium text-pink-600">
-            {{ opening.role }}
-          </div>
-          <div class="text-gray-700">{{ opening.description }}</div>
-        </div>
-        <div class="flex flex-col basis-1/4 justify-around items-center">
-          <div class="flex gap-2 p-2 text-gray-700 items-center w-full">
-            <IconCalendar class="h-5 w-5" /> {{ opening.experience }}
-          </div>
-          <div class="flex gap-2 p-2 text-gray-700 items-center w-full">
-            <IconHourglass class="h-5 w-5" /> {{ opening.employmentType }}
-          </div>
-          <div class="flex gap-2 p-2 text-gray-700 items-center w-full">
-            <IconMapPin class="h-5 w-5" /> {{ opening.location }}
+          <div class="flex basis-1/4 flex-col items-center justify-around">
+            <div class="flex w-full items-center gap-2 p-2 text-gray-700">
+              <IconCalendar class="h-5 w-5" /> {{ opening.experience }}
+            </div>
+            <div class="flex w-full items-center gap-2 p-2 text-gray-700">
+              <IconHourglass class="h-5 w-5" /> {{ opening.employmentType }}
+            </div>
+            <div class="flex w-full items-center gap-2 p-2 text-gray-700">
+              <IconMapPin class="h-5 w-5" /> {{ opening.location }}
+            </div>
+            <button
+              class="flex w-full items-center justify-center gap-2 rounded-lg bg-pink-600 px-4 py-2 text-white"
+              @click="applyTo(opening.id)"
+            >
+              Apply <IconCircleArrowRight class="h-5 w-5" />
+            </button>
           </div>
         </div>
       </div>
     </div>
+    <Dialog
+      :open="isApplyDialogOpen"
+      @close="isApplyDialogOpen = false"
+      class="relative z-50"
+    >
+      <div class="fixed inset-0 bg-black/30" aria-hidden="true"></div>
+      <div class="fixed inset-0 flex w-screen items-center justify-center p-4">
+        <DialogPanel class="w-full max-w-lg rounded bg-white p-4">
+          <DialogTitle class="mb-3 text-2xl font-medium">
+            Apply for opening
+          </DialogTitle>
+          <div>
+            <FormKit
+              id="application-form"
+              type="form"
+              :actions="false"
+              v-model="formData"
+              @submit="handleSubmit"
+            >
+              <FormKit
+                type="file"
+                name="cv_url"
+                label="CV"
+                accept=".pdf"
+                validation="required"
+                :disabled="isProcessing"
+              />
+              <div class="flex w-full gap-3">
+                <div class="grow">
+                  <FormKit
+                    type="text"
+                    name="firstname"
+                    label="Firstname"
+                    placeholder="Firstname"
+                    validation="required"
+                    :disabled="isProcessing"
+                  />
+                </div>
+                <div class="grow">
+                  <FormKit
+                    type="text"
+                    name="lastname"
+                    label="Lastname"
+                    placeholder="Lastname"
+                    validation="required"
+                    :disabled="isProcessing"
+                  />
+                </div>
+              </div>
+              <FormKit
+                type="email"
+                name="email"
+                label="Email"
+                placeholder="Email"
+                validation="required"
+                :disabled="isProcessing"
+              />
+              <FormKit
+                type="tel"
+                name="phone"
+                label="Phone Number"
+                placeholder="Phone Number"
+                validation="required"
+                :disabled="isProcessing"
+              />
+              <FormKit
+                type="textarea"
+                name="message"
+                label="Message"
+                placeholder="Eg. I would like to book a lesson"
+                :disabled="isProcessing"
+              />
+              <div>
+                <button
+                  type="submit"
+                  class="flex w-full items-center justify-center rounded-lg bg-pink-600 px-4 py-2 uppercase text-white"
+                  :disabled="isProcessing"
+                >
+                  <div v-if="isProcessing">
+                    <SpinnerComponent class="h-6 w-6" />
+                  </div>
+                  <div v-else>Submit</div>
+                </button>
+              </div>
+            </FormKit>
+          </div>
+        </DialogPanel>
+      </div>
+    </Dialog>
+    <Dialog
+      :open="isSuccessDialogOpen"
+      @close="isSuccessDialogOpen = false"
+      class="relative z-50"
+    >
+      <div class="fixed inset-0 bg-black/30" aria-hidden="true"></div>
+      <div class="fixed inset-0 flex w-screen items-center justify-center p-4">
+        <DialogPanel class="w-full max-w-sm rounded bg-white p-4">
+          <DialogTitle class="mb-3 text-2xl font-medium">
+            Application received
+          </DialogTitle>
+          <DialogDescription>
+            We have received your application and will get back to you shortly.
+          </DialogDescription>
+          <button
+            @click="isSuccessDialogOpen = false"
+            class="mt-5 w-full rounded-lg bg-pink-600 px-4 py-2 uppercase text-white"
+          >
+            Ok
+          </button>
+        </DialogPanel>
+      </div>
+    </Dialog>
   </div>
 </template>
