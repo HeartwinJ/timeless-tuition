4d91d927e258827a346cb435696fb9eba32dcf5d
Heartwin Haveluck <heartwinhaveluck@gmail.com>
2024-02-02 13:48:28 +0000

    Updated news

--- src/components/ScrollerComponent.vue
+++ src/components/ScrollerComponent.vue
@@ -1,5 +1,10 @@
 <template>
-	<div class="p-3 font-bold text-white bg-red-600">
-		<marquee>Test Test Test Test</marquee>
-	</div>
+  <div class="bg-brand p-3 font-bold text-white">
+    <marquee>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, qui
+      labore inventore harum, ipsum veniam corrupti officiis ipsam mollitia
+      suscipit vero at! Quaerat esse modi, architecto provident officia
+      aspernatur voluptatibus!
+    </marquee>
+  </div>
 </template>
